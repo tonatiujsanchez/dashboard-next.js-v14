65ac564f29e73df4b5db1561fd42177db6d6a462
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2024-05-27 23:29:06 -0600

    Página de Detalles de Pokemon, añadida

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -29,7 +29,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
                             <p className="pt-2 text-lg font-semibold text-gray-50">{ name }</p>
                             <div className="mt-5">
                                 <Link
-                                    href={`/hello/pokemon/${id}`}
+                                    href={`/dashboard/pokemon/${id}`}
                                     prefetch={true}
                                     className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
                                 >
@@ -38,20 +38,19 @@ export const PokemonCard = ({ pokemon }: Props) => {
                             </div>
                         </div>
                         <div className="border-b">
-                            <Link
-                                href={`/dashboard/main`}
-                                className="px-4 py-2 hover:bg-gray-100 flex"
+                            <button
+                                className="w-full px-4 py-2 hover:bg-gray-100 flex items-center gap-3"
                             >
                                 <div className="text-red-600">
                                     <IoHeartOutline size={24} />
                                 </div>
-                                <div className="pl-3">
+                                <div className="flex flex-col items-start">
                                     <p className="text-sm font-medium text-gray-800 leading-none">
                                         Agregar a favoritos
                                     </p>
                                     <p className="text-xs text-gray-500">View your campaigns</p>
                                 </div>
-                            </Link>
+                            </button>
                         </div>
                     </div>
                 </div>
